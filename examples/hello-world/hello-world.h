684e5d42d36eff93048849c35b9fc195e0d92c13
arun1587 <arunprabhu.1@gmail.com>
2015-11-29 02:39:15 +0100

    added header

---
+++ examples/hello-world/hello-world.h
@@ -0,0 +1,5 @@
+void hello_world_init();
+extern struct process hello_world_process;
+~                                                                                                                                             
+~                                                                                                                                             
+~                                              
